0b72e257d8823eb4ffba7c4b3dcf33984ae4e2d9
thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2020-12-06 14:07:18 -0300

    adicionando comentários a questao 3

--- Questoes/src/questao03/Mesa.java
+++ Questoes/src/questao03/Mesa.java
@@ -14,14 +14,21 @@ public class Mesa {
 		this.pessoas += 1;
 		System.out.println("Aluno " + name + " entrou no mesa");
 		
+		/* Se ainda não tiver pelo menos 2 alunos na mesa, espera, para que não possa 
+		 * beber sozinho.
+		*/
 		while (this.pessoas < 2) {
 			wait();
 		}
 		
+		/* Quando a quantidade de pessoas na mesa atingir 2, notificar a outra
+		 *  thread que ficou esperando para poder começar a beber
+		 */
 		if (this.pessoas == 2 )  
 			notifyAll();		
 	}
 	
+	// Aluno começou a beber
 	public void bebe(String name) throws InterruptedException {
 		System.out.println("Aluno " + name + " está bebendo");
 		this.pessoasBebendo += 1;
@@ -29,17 +36,19 @@ public class Mesa {
 		this.remediado(name);
 	}
 	
+	// Aluno terminou de beber
 	public void remediado(String name) {
 		System.out.println("Aluno " + name + " está remediado");
 		this.pessoasBebendo -= 1;
 	}
 	
+	// verifica se aluno pode sair, de acordo com a restrinção.
 	private boolean naoPodeSair() {
 		return (this.pessoasBebendo == 2 && this.pessoas <= 2);
 	}
 	
 	public synchronized void sai(String name) throws InterruptedException {
-
+		// Se o aluno poder sair, então sai da mesa, e espera.
 		while (!this.naoPodeSair()) {
 			System.out.println("Aluno " + name + " saiu da mesa");
 			this.pessoas -= 1;
